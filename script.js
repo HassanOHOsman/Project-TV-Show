b4b24b82ff2c3b6da8c995f035f2ca093830c3b7
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-07-23 14:43:09 +0100

    added episode code (mix of name & season)

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ function makePageForEpisodes(episodeList) {
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   episodeList.forEach(episode => {
     const eachEpisode = document.createElement("div");
+    const episodeCode = "S" + String(episode.season).padStart(2, "0") + "E" + String(episode.number).padStart(2, "0");
     eachEpisode.innerHTML = `
-      <h3>${episode.name}</h3>
+      <h3>${episode.name} - ${episodeCode}</h3>
       <img src="${episode.image.medium}" alt="Thumbnail for ${episode.name} - Episode ${episode.number} - Season ${episode.season}">
       <p>${episode.summary}</p>`;
 
