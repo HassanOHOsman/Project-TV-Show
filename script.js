f7b698438e17b9c9c60bbeaee9e788cd3d6ac1ab
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-07-23 14:22:38 +0100

    displayed name, image & summary for all episodes

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  episodeList.forEach( episode => {
+  episodeList.forEach(episode => {
     const eachEpisode = document.createElement("div");
+    eachEpisode.innerHTML = `
+      <h3>${episode.name}</h3>
+      <img src="${episode.image.medium}" alt="Thumbnail for ${episode.name} - Episode ${episode.number} - Season ${episode.season}">
+      <p>${episode.summary}</p>`;
 
+    rootElem.appendChild(eachEpisode);
   });
 }
 
