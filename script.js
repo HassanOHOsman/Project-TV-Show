d8bf6dff5ff4caed033760e9415d605779d9a130
sheida-shab <shabankary@gmail.com>
2025-07-26 12:19:13 +0100

    Refactor code: clear root before rendering,  better naming

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  displayEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
+function displayEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
   episodeList.forEach(episode => {
-    const eachEpisode = document.createElement("div");
+    const divEpisode = document.createElement("div");
     const episodeCode = "S" + String(episode.season).padStart(2, "0") + "E" + String(episode.number).padStart(2, "0");
-    eachEpisode.innerHTML = `
+    divEpisode.innerHTML = `
       <h3>${episode.name} - ${episodeCode}</h3>
       <img src="${episode.image.medium}" alt="Thumbnail for ${episode.name} - Episode ${episode.number} - Season ${episode.season}">
       <p>${episode.summary}</p>`;
 
-    rootElem.appendChild(eachEpisode);
+    rootElem.appendChild(divEpisode);
   });
 }
 
