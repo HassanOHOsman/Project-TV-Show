4159f8ec2d82f0629068a25b83ea622979eb4197
sheida-shab <shabankary@gmail.com>
2025-07-26 13:31:55 +0100

    Live search input(name or summary)-case insensitive-immediately

--- script.js
+++ script.js
@@ -2,15 +2,32 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   displayEpisodes(allEpisodes);
+  const searchBox = document.getElementById("searchInput");
+  searchBox.addEventListener("input", function () {
+    const searchItem = searchBox.value.toLowerCase();
+    const filteredEpisodes = allEpisodes.filter((episode) => {
+      const episodeName = episode.name.toLowerCase();
+      const episodeSummaryText = episode.summary.toLowerCase();
+      return (
+        episodeName.includes(searchItem) ||
+        episodeSummaryText.includes(searchItem)
+      );
+    });
+    displayEpisodes(filteredEpisodes);
+  });
 }
 
 function displayEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   rootElem.innerHTML = "";
-  episodeList.forEach(episode => {
+  episodeList.forEach((episode) => {
     const divEpisode = document.createElement("div");
-    const episodeCode = "S" + String(episode.season).padStart(2, "0") + "E" + String(episode.number).padStart(2, "0");
+    const episodeCode =
+      "S" +
+      String(episode.season).padStart(2, "0") +
+      "E" +
+      String(episode.number).padStart(2, "0");
     divEpisode.innerHTML = `
       <h3>${episode.name} - ${episodeCode}</h3>
       <img src="${episode.image.medium}" alt="Thumbnail for ${episode.name} - Episode ${episode.number} - Season ${episode.season}">
@@ -33,6 +50,5 @@ hyperLink.textContent = "TVmaze";
 footerText.appendChild(hyperLink);
 pageFooter.appendChild(footerText);
 document.body.appendChild(pageFooter);
-
 
 window.onload = setup;
